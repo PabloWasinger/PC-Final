9e2a959be0187f01182d040b61fb2a77bc15ba20
Felisl <felipesidiropulos04@gmail.com>
2023-11-16 13:26:32 -0300

    comiteado

--- vehiculo_bien_definido.py
+++ vehiculo_bien_definido.py
@@ -27,4 +27,4 @@ class Vehiculos():
             x_offset, y_offset, z_offset = funciones.get_coords()
             posicion  = (x >= size[0] + x_offset) & (x < size[1] + x_offset) & (y >= size[2] + y_offset) (y < size[3] + y_offset) & (z >= size[4] + z_offset) (z < size[5] + z_offset)
             colision = funciones.check_collision(posicion)
-        
+ytrcuy = 1      
